3558ce86850145e1c8aea13256ce5911ee3c864d
weiminlee1 <weiminlee1@gmail.com>
2019-05-07 10:50:00 +0800

    Update 数组3.py

--- 数据分析/数组3.py
+++ 数据分析/数组3.py
@@ -13,4 +13,13 @@ plt.show()
 #########################
 ##DataFrame
 data = pd.DataFrame(np.random.randn(1000,4), index=np.arange(1000),
-columns=list('ABCD')
+                    columns=list('ABCD'))
+                
+data = data.cumsum()
+data.plot()
+plt.show()
+
+##'bar','hist','box','kde','area','scatter','hexbin','pie'
+ax = data.plot.scatter(x='A',y='B',color='DarkBlue',label='Class1')
+data.plot.scatter(x='A',y='C',color='DarkGreen',label='Class2',ax=ax) ##一个画板显示两个图
+plt.show()
