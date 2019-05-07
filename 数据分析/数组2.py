35a69a302835e3992c35523dc5a6bbd1012fcb10
weiminlee1 <weiminlee1@gmail.com>
2019-05-07 10:05:45 +0800

    Update 数组2.py

--- 数据分析/数组2.py
+++ 数据分析/数组2.py
@@ -32,7 +32,7 @@ df.describe() ##输出df的count,mean,min,max,std等
 df.sort_index(axis=1,ascending=False) ##按行进行倒序排列
 df.sort_index(axis=0,ascending=False) ##按列索引进行倒序排列
 df.sort_values(by='a') ##安某列值进行排列
-
+备注：1代表左右方向的维度，0代表上下方向的维度
 ######################################################################################
 pandas 选择数据
 
@@ -67,9 +67,42 @@ pd.read_csv(options)
  
 pd.to_csv(options)
 
+##############################################################################
+数据框合并
 res = pd.concat([df1,df2,df3],axix=0/1) #数组合并，index,columns要一样
 
 res = pd.concat([df1,df2],join='inner',ignore_index=True) 合并df1,df2适合index不一样
 
-res = pd.concat([df1,df2], axis=1, join_axes=[df1.index]) ##
+res = pd.concat([df1,df2], axis=1, join_axes=[df1.index]) ##以df1的index为准，df2中缺失的
+index由NaN填充
+
+res = df1.append(df2, ignore_index=True) ##在上下方向添加
+res = df1.append([df2,df3],ignore_index=True) ##上下方向添加
+
+s1 = pd.Series([1,2,3,4], index=['a','b','c','d'])
+res = df1.append(s1,ignore_index=True)
+
+##########################################################################
+left = pd.DataFrame({'key':['K0','K1','K2','K3'],'A':['A0','A1','A2','A3'],
+                     'B':['B0','B1','B2','B3']})
+right = pd.DataFrame({'key':['K0','K1','K2','K3'],'C':['C0','C1','C2','C3'],
+                      'D':['D0','D1','D2','D3']})
+
+res = pd.merge(left,right,on='key') ##合并left，right两个数据框left.key=right.key
+res = pd.merge(left,right,on=['key1','key2'],how='inner') ##left.key1/key2 = right.key1/key2
+##how='inner','outer','left','right'
+res = pd.merge(df1,df2,on='col1',how='outer',indicator=True) ##显示合并数据的动态过程
+
+res = pd.merge(left,right,left_index=True,right_index=True,how='outer') ##按index进行合并
+
+boys = pd.DataFrame({'k':['k0','k1','k2'],'age':[1,2,3]})
+girls = pd.DataFrame({'k':['k0','k1','k3'],'age':[4,6,7]})
+
+res = pd.merge(boys,girls, on='k',suffixes=['_boy','_girl'],how='inner')
+##inner只显示共同项的，outer则显示全部的
+
+
+ 
+
+
 
